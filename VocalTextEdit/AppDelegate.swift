f64eddfb2f5399c8b4ddf0c9798a5f5b1496dc00
MichaelWMoore <mwmoore808@hotmail.com>
2019-12-26 17:33:36 -0700

    Initial Commit

---
+++ VocalTextEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VocalTextEdit
+//
+//  Created by Michael W. Moore on 12/26/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by Michael W. Moore on 12/26/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
